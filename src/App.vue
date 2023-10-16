ee50f35c11b2e54a34095eeaed4d4738218da819
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-10-16 13:03:39 +0300

    decomp-third-screen

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import ScreenFirst from "./components/screenFirst.vue"
 import SecondScreen from "./components/SecondScreen.vue"
-import MessangerIcon from "./components/Images/MessangerIcon.vue"
-import SendMessageIcon from "./components/Images/SendMessageIcon.vue"
-import FIlterIcon from "./components/Images/FIlterIcon.vue"
+// import MessangerIcon from "./components/Images/MessangerIcon.vue"
+// import SendMessageIcon from "./components/Images/SendMessageIcon.vue"
+// import FIlterIcon from "./components/Images/FIlterIcon.vue"
+import ScreenThird from "./components/ScreenThird.vue"
 </script>
 
 <template>
@@ -11,58 +12,7 @@ import FIlterIcon from "./components/Images/FIlterIcon.vue"
   <main>
     <ScreenFirst />
     <SecondScreen />
-    <section id="sectionThird">
-      <div class="container">
-        <div class="d-flex flex-column align-items-center text-center mb-5">
-          <MessangerIcon />
-          <h2 class="fs-3 fw-bold">
-            Capture leads and accelerate sales via messengers
-          </h2>
-          <p class="p-4 fs-6 text-gray-500">
-            Link your ads, website and social media profiles directly to a
-            conversation, and help to simplify the buying process for both you
-            and your customers.
-          </p>
-          <img
-            src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da07944d2c9cbb2c545bc8_img-2-en.png"
-            loading="lazy"
-            alt=""
-            class="w-[240px]"
-          />
-        </div>
-        <div class="d-flex flex-column align-items-center text-center mb-5">
-          <SendMessageIcon />
-          <h2 class="fs-3 fw-bold">Send messages with an 80% open rate</h2>
-          <p class="p-4 fs-6 text-gray-500">
-            Segment your audience using tags and attributes and broadcast
-            personalized messages in just a few clicks. The messenger channel
-            works much better than email, and it allows you to initiate a
-            conversation and close deals within the chat itself.
-          </p>
-          <img
-            src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da07944d2c9cbb2c545bc8_img-2-en.png"
-            loading="lazy"
-            alt=""
-            class="w-[240px]"
-          />
-        </div>
-        <div class="d-flex flex-column align-items-center text-center mb-5">
-          <FIlterIcon />
-          <h2 class="fs-3 fw-bold">Automate sales funnels</h2>
-          <p class="p-4 fs-6 text-gray-500">
-            Build sequences and chatbots to automate the sales or lead
-            qualification process – without coding. Bots can help you give a
-            coupon, ask common questions or share content.
-          </p>
-          <img
-            src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da07944d2c9cbb2c545bc8_img-2-en.png"
-            loading="lazy"
-            alt=""
-            class="w-[240px]"
-          />
-        </div>
-      </div>
-    </section>
+    <ScreenThird />
   </main>
 </template>
 
