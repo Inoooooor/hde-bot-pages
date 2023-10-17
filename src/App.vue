d7595088b09d5248c869ef98347f256654862b44
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-10-17 15:35:39 +0300

    PlanCardComp-init

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import ScreenFirst from "./components/ScreenFirst.vue"
 import SecondScreen from "./components/SecondScreen.vue"
 import ScreenThird from "./components/ScreenThird.vue"
 import ScreenFourth from "./components/ScreenFourth.vue"
-// import ScreenFivthTabs from "./components/UI/ScreenFivthTabs.vue"
 import ScreenFivth from "./components/ScreenFivth.vue"
+import PlanCard from "./components/UI/PlanCard.vue"
 </script>
 
 <template>
@@ -15,12 +15,29 @@ import ScreenFivth from "./components/ScreenFivth.vue"
     <ScreenThird />
     <ScreenFourth />
     <ScreenFivth />
-    <section class="min-vh-100"></section>
+    <section class="background-solid">
+      <div class="container">
+        <div class="row">
+          <div class="text-center">
+            <h2 class="fs-3 fw-bold">Choose a plan that's right for you</h2>
+            <p class="py-4 fs-6 text-gray-500">
+              You start for free and pay as you go, with honest pricing that
+              scales as you grow. BotHelp accounts are billed based on the
+              number of active subscribers. Tools, messages and agents are
+              unlimited.
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="d-flex flex-column">
+            <PlanCard plan-type="free" />
+            <div>card pro</div>
+            <div>card business</div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
-<style scoped>
-.background-solid {
-  background-color: #23869a10;
-}
-</style>
+<style scoped></style>
---
+++ src/components/UI/PlanCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-card
+    body-class="px-4"
+    class="border-2 hover:border-[#23879A]"
+  >
+    <template #header>
+      <div class="text-center d-flex flex-column">
+        <h3 class="fs-2 fw-bold">{{ plans[planType].header }}</h3>
+        <p class="fs-6 text-gray-500 py-4">
+          Free 14-days trial with access to all features
+        </p>
+        <div class="fs-1 fw-bold">{{ plans[planType].price }}</div>
+      </div>
+    </template>
+    <ul>
+      <li
+        v-for="(item, index) in plans[planType].planList"
+        :key="index"
+        class="d-flex align-items-center mb-3 text-gray-500 fs-6"
+      >
+        <div class="w-[6px] h-[6px] bg-[#23879A] rounded me-2"></div>
+        {{ item }}
+      </li>
+    </ul>
+  </el-card>
+</template>
+
+<script setup>
+const props = defineProps({
+  planType: {
+    type: String,
+    required: true,
+    validator(value) {
+      return ["free", "pro", "business"].includes(value)
+    },
+  },
+})
+
+const plans = {
+  free: {
+    price: "0$",
+    header: "Free",
+    planList: [
+      "Up to 3000 subscribers",
+      "Up to 10 agents",
+      "Unlimited Flows, Chatbots and Growth Tools",
+      "All core features included.",
+      "FB Messenger, Telegram, Viber",
+    ],
+  },
+}
+console.log(props.planType)
+</script>
+
+<style scoped>
+.el-card {
+  border-radius: 10px;
+}
+</style>
