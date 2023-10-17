e4ba23eda713991a3046422f420e26d289095735
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-10-17 13:58:45 +0300

    decomp-mobile-tab

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,7 @@ import ScreenFirst from "./components/ScreenFirst.vue"
 import SecondScreen from "./components/SecondScreen.vue"
 import ScreenThird from "./components/ScreenThird.vue"
 import ScreenFourth from "./components/ScreenFourth.vue"
-
-import { ref } from "vue"
-const activeName = ref("Broadcasting")
+import ScreenFivthTabs from "./components/UI/ScreenFivthTabs.vue"
 </script>
 
 <template>
@@ -21,63 +19,12 @@ const activeName = ref("Broadcasting")
     >
       <div class="container">
         <div class="row">
-          <div class="d-flex">
+          <div class="d-flex py-2">
             <h2 class="fs-3 fw-bold">How does it work?</h2>
           </div>
         </div>
         <div class="row">
-          <el-tabs
-            v-model="activeName"
-            class="demo-tabs"
-          >
-            <el-tab-pane
-              label="Broadcasting"
-              name="Broadcasting"
-            >
-              <div class="text-center">
-                <p class="py-4 fs-6 text-gray-500">
-                  BotHelp helps businesses increase sales and build close
-                  relationships with customers via Facebook Messenger by
-                  providing powerful automation tools. Chatting, broadcasting
-                  and bots are all in one place!
-                </p>
-                <div class="d-flex justify-content-center pb-4">
-                  <img
-                    src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da172050951638bc6cd5ea_tab-01-en.jpg"
-                    loading="lazy"
-                    sizes="90vw"
-                    width="980"
-                    alt=""
-                    srcset="
-                      https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da172050951638bc6cd5ea_tab-01-en-p-800.jpeg 800w,
-                      https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da172050951638bc6cd5ea_tab-01-en.jpg        980w
-                    "
-                    class="position-static shadow"
-                  />
-                </div>
-              </div>
-            </el-tab-pane>
-            <el-tab-pane
-              label="Analytics"
-              name="Analytics"
-              >Config
-            </el-tab-pane>
-            <el-tab-pane
-              label="Flow Builder"
-              name="Flow Builder"
-              >Role
-            </el-tab-pane>
-            <el-tab-pane
-              label="Conversations"
-              name="Conversations"
-              >Task
-            </el-tab-pane>
-            <el-tab-pane
-              label="Subscriber list"
-              name="Subscriber list"
-              >Task
-            </el-tab-pane>
-          </el-tabs>
+          <ScreenFivthTabs />
         </div>
       </div>
     </section>
---
+++ src/components/UI/ScreenFivthTabs.vue
@@ -0,0 +1,159 @@
+<template>
+  <el-tabs
+    v-model="activeName"
+    class="section-fivth--tabs"
+  >
+    <el-tab-pane
+      label="Broadcasting"
+      name="Broadcasting"
+    >
+      <div class="text-center">
+        <p class="py-4 fs-6 text-gray-500">
+          BotHelp helps businesses increase sales and build close relationships
+          with customers via Facebook Messenger by providing powerful automation
+          tools. Chatting, broadcasting and bots are all in one place!
+        </p>
+        <div class="d-flex justify-content-center pb-4">
+          <img
+            src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da172050951638bc6cd5ea_tab-01-en.jpg"
+            loading="lazy"
+            sizes="90vw"
+            width="980"
+            alt=""
+            srcset="
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da172050951638bc6cd5ea_tab-01-en-p-800.jpeg 800w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da172050951638bc6cd5ea_tab-01-en.jpg        980w
+            "
+            class="position-static shadow"
+          />
+        </div>
+      </div>
+    </el-tab-pane>
+    <el-tab-pane
+      label="Analytics"
+      name="Analytics"
+    >
+      <div class="text-center">
+        <p class="py-4 fs-6 text-gray-500">
+          Track the growth of the subscriber list. Analyze the effectiveness of
+          advertising campaigns by UTM tags.
+        </p>
+        <div class="d-flex justify-content-center pb-4">
+          <img
+            src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1720d75a7372dff47a10_tab-02-en.jpg"
+            loading="lazy"
+            width="980"
+            sizes="90vw"
+            alt=""
+            srcset="
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1720d75a7372dff47a10_tab-02-en-p-500.jpeg 500w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1720d75a7372dff47a10_tab-02-en-p-800.jpeg 800w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1720d75a7372dff47a10_tab-02-en.jpg        980w
+            "
+            class="position-static shadow"
+          />
+        </div>
+      </div>
+    </el-tab-pane>
+    <el-tab-pane
+      label="Flow Builder"
+      name="Flow Builder"
+    >
+      <div class="text-center">
+        <p class="py-4 fs-6 text-gray-500">
+          Build chatbots or automated drill campaigns in a beautiful visual
+          builder. No coding required.
+        </p>
+        <div class="d-flex justify-content-center pb-4">
+          <img
+            src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da17216e72f50b89967e98_tab-03-en.jpg"
+            loading="lazy"
+            width="980"
+            sizes="90vw"
+            alt=""
+            srcset="
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da17216e72f50b89967e98_tab-03-en-p-800.jpeg   800w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da17216e72f50b89967e98_tab-03-en-p-1080.jpeg 1080w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da17216e72f50b89967e98_tab-03-en-p-1600.jpeg 1600w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da17216e72f50b89967e98_tab-03-en.jpg         1870w
+            "
+            class="position-static shadow"
+          />
+        </div>
+      </div>
+    </el-tab-pane>
+    <el-tab-pane
+      label="Conversations"
+      name="Conversations"
+    >
+      <div class="text-center">
+        <p class="py-4 fs-6 text-gray-500">
+          Make yourself available to your customers, with little or no effort on
+          their part. Let your customers ask questions directly through
+          Messenger in real time, personally and privately.
+        </p>
+        <div class="d-flex justify-content-center pb-4">
+          <img
+            src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1722a0df361cafd45790_tab-04-en.jpg"
+            loading="lazy"
+            width="980"
+            sizes="90vw"
+            alt=""
+            srcset="
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1722a0df361cafd45790_tab-04-en-p-500.jpeg   500w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1722a0df361cafd45790_tab-04-en-p-800.jpeg   800w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1722a0df361cafd45790_tab-04-en-p-1080.jpeg 1080w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1722a0df361cafd45790_tab-04-en-p-1600.jpeg 1600w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1722a0df361cafd45790_tab-04-en.jpg         1858w
+            "
+            class="position-static shadow"
+          />
+        </div>
+      </div>
+    </el-tab-pane>
+    <el-tab-pane
+      label="Subscriber list"
+      name="Subscriber list"
+    >
+      <div class="text-center">
+        <p class="py-4 fs-6 text-gray-500">
+          Manage your contact list in different messengers and edit your profile
+          data. Use filters, export and import subscribers.
+        </p>
+        <div class="d-flex justify-content-center pb-4">
+          <img
+            src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1721b04d99bca3e6c439_tab-05-en.jpg"
+            loading="lazy"
+            sizes="90vw"
+            srcset="
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1721b04d99bca3e6c439_tab-05-en-p-500.jpeg 500w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1721b04d99bca3e6c439_tab-05-en-p-800.jpeg 800w,
+              https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60da1721b04d99bca3e6c439_tab-05-en.jpg        863w
+            "
+            alt=""
+            class="position-static shadow"
+          />
+        </div>
+      </div>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script setup>
+import { ref } from "vue"
+const activeName = ref("Broadcasting")
+</script>
+
+<style>
+.section-fivth--tabs .el-tabs__item {
+  font-size: 1rem;
+}
+
+.section-fivth--tabs .el-tabs__item.is-active {
+  color: #23879a;
+}
+
+.section-fivth--tabs .el-tabs__active-bar {
+  background-color: #23879a;
+}
+</style>
