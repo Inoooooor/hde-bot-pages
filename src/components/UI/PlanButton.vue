ce186226b0a1d8ec6df79b53cf4617c934123ec7
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-10-18 11:13:33 +0300

    added_plan_btn

---
+++ src/components/UI/PlanButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-button
+    color="#23879A"
+    class="w-100 fs-4 text-white py-4"
+    >{{ buttonText }}
+  </el-button>
+</template>
+
+<script setup>
+defineProps({
+  buttonText: {
+    type: String,
+    required: true,
+    default: "Start Trial",
+    validator(value) {
+      return ["Start Trial", "Contact Us"].includes(value)
+    },
+  },
+})
+</script>
+
+<style scoped></style>
