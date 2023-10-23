88fb70737c5d0672b7a31aac10159ecebd4b8e67
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-10-23 14:30:38 +0300

    fixed-hero-button-hover

--- src/components/UI/HeroButton.vue
+++ src/components/UI/HeroButton.vue
@@ -2,7 +2,7 @@
   <el-button
     color="#23879A"
     plain
-    class="col-7 fs-5 p-4 rounded-3"
+    class="col-7 fs-5 p-4 rounded-3 border"
     >Get started for free
   </el-button>
 </template>
