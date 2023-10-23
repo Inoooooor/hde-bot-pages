396dab91f1f943a6cf419fc499090871f0ec4433
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-10-23 10:14:27 +0300

    why-we-section-adaptive-no-fontsizes-yet

--- src/components/ScreenFourth.vue
+++ src/components/ScreenFourth.vue
@@ -2,97 +2,102 @@
   <section id="screenFourth">
     <div class="background2">
       <div class="container">
-        <div
-          class="d-flex flex-column align-items-center text-center text-white pt-5 mb-4"
-        >
-          <StarIcon />
-          <h2 class="fs-3 fw-bold">Why HelpDeskEddy?</h2>
-          <p class="py-4 fs-6">
-            HelpDeskEddy is an all-in-one platform that helps you sell on
-            WhatsApp, Instagram, Facebook, Telegram and Viber. It’s ideal for
-            marketers and digital product owners.
-          </p>
-        </div>
-        <div class="d-flex flex-column text-white">
-          <div class="d-flex mb-4">
-            <img
-              src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc74917f4df790f25e65e8_i-sm-01.svg"
-              loading="lazy"
-              alt=""
-              class="me-4 w-[45px] h-[45px]"
-            />
-            <div>
-              <h3 class="fs-6 fw-bold mb-2">Multi-channel chatbot builder</h3>
-              <p class="">The visual and intuitive no-code chatbot builder.</p>
-            </div>
+        <div class="d-flex flex-column flex-lg-row-reverse">
+          <div
+            class="d-flex flex-column align-items-center text-center text-white pt-5 mb-4 col-lg-4 justify-content-lg-center"
+          >
+            <StarIcon />
+            <h2 class="fs-3 fw-bold">Why HelpDeskEddy?</h2>
+            <p class="py-4 fs-6">
+              HelpDeskEddy is an all-in-one platform that helps you sell on
+              WhatsApp, Instagram, Facebook, Telegram and Viber. It’s ideal for
+              marketers and digital product owners.
+            </p>
           </div>
-          <div class="d-flex mb-4">
-            <img
-              src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc7491b03c622f2db7a7e8_i-sm-03.svg"
-              loading="lazy"
-              alt=""
-              class="me-4 w-[45px] h-[45px]"
-            />
-            <div>
-              <h3 class="fs-6 fw-bold mb-2">Subscriber base</h3>
-              <p class="">Segment and manage your subscriber base.</p>
+          <div
+            class="d-flex flex-column flex-lg-row text-white col-lg-8 flex-lg-wrap justify-content-lg-around"
+          >
+            <div class="d-flex mb-4 mb-lg-0 py-lg-5 col-lg-5">
+              <img
+                src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc74917f4df790f25e65e8_i-sm-01.svg"
+                loading="lazy"
+                alt=""
+                class="me-4 w-[45px] h-[45px]"
+              />
+              <div class="d-flex flex-column justify-content-center">
+                <h3 class="fs-6 fw-bold mb-2">Multi-channel chatbot builder</h3>
+                <p class="">
+                  The visual and intuitive no-code chatbot builder.
+                </p>
+              </div>
             </div>
-          </div>
-          <div class="d-flex mb-4">
-            <img
-              src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc7491521f5d2664de3a60_i-sm-05.svg"
-              loading="lazy"
-              alt=""
-              class="me-4 w-[45px] h-[45px]"
-            />
-            <div>
-              <h3 class="fs-6 fw-bold mb-2">Growth tools</h3>
-              <p class="">
-                Ready-made growth tools for websites, Instagram and Facebook to
-                help acquire new subscribers.
-              </p>
+            <div class="d-flex mb-4 mb-lg-0 py-lg-5 col-lg-5">
+              <img
+                src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc7491b03c622f2db7a7e8_i-sm-03.svg"
+                loading="lazy"
+                alt=""
+                class="me-4 w-[45px] h-[45px]"
+              />
+              <div class="d-flex flex-column justify-content-center">
+                <h3 class="fs-6 fw-bold mb-2">Subscriber base</h3>
+                <p class="">Segment and manage your subscriber base.</p>
+              </div>
             </div>
-          </div>
-          <div class="d-flex mb-4">
-            <img
-              src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc74919f1dda72666e437d_i-sm-02.svg"
-              loading="lazy"
-              alt=""
-              class="me-4 w-[45px] h-[45px]"
-            />
-            <div>
-              <h3 class="fs-6 fw-bold mb-2">Broadcasts</h3>
-              <p class="">Engage with prospects and send tailored content.</p>
+            <div class="d-flex mb-4 mb-lg-0 py-lg-5 col-lg-5">
+              <img
+                src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc7491521f5d2664de3a60_i-sm-05.svg"
+                loading="lazy"
+                alt=""
+                class="me-4 w-[45px] h-[45px]"
+              />
+              <div class="d-flex flex-column justify-content-center">
+                <h3 class="fs-6 fw-bold mb-2">Growth tools</h3>
+                <p class="">
+                  Ready-made growth tools for websites, Instagram and Facebook
+                  to help acquire new subscribers.
+                </p>
+              </div>
             </div>
-          </div>
-          <div class="d-flex mb-4">
-            <img
-              src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc74919f1ddab1ce6e4383_i-sm-04.svg"
-              loading="lazy"
-              alt=""
-              class="me-4 w-[45px] h-[45px]"
-            />
-            <div>
-              <h3 class="fs-6 fw-bold mb-2">Analytics</h3>
-              <p class="">
-                Get detailed analytics on conversions and traffic sources.
-              </p>
+            <div class="d-flex mb-4 mb-lg-0 py-lg-5 col-lg-5">
+              <img
+                src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc74919f1dda72666e437d_i-sm-02.svg"
+                loading="lazy"
+                alt=""
+                class="me-4 w-[45px] h-[45px]"
+              />
+              <div class="d-flex flex-column justify-content-center">
+                <h3 class="fs-6 fw-bold mb-2">Broadcasts</h3>
+                <p class="">Engage with prospects and send tailored content.</p>
+              </div>
             </div>
-          </div>
-          <div class="d-flex mb-4">
-            <img
-              src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc74910261301164fda893_i-sm-06.svg"
-              loading="lazy"
-              alt=""
-              class="me-4 w-[45px] h-[45px]"
-            />
-            <div>
-              <h3 class="fs-6 fw-bold mb-2">
-                Get detailed analytics on conversions and traffic sources.
-              </h3>
-              <p class="">
-                Respond to clients in different messengers from a single window.
-              </p>
+            <div class="d-flex mb-4 mb-lg-0 py-lg-5 col-lg-5">
+              <img
+                src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc74919f1ddab1ce6e4383_i-sm-04.svg"
+                loading="lazy"
+                alt=""
+                class="me-4 w-[45px] h-[45px]"
+              />
+              <div class="d-flex flex-column justify-content-center">
+                <h3 class="fs-6 fw-bold mb-2">Analytics</h3>
+                <p class="">
+                  Get detailed analytics on conversions and traffic sources.
+                </p>
+              </div>
+            </div>
+            <div class="d-flex mb-4 mb-lg-0 py-lg-5 col-lg-5">
+              <img
+                src="https://uploads-ssl.webflow.com/60cb5213aeb6e63bf7b6b47a/60cc74910261301164fda893_i-sm-06.svg"
+                loading="lazy"
+                alt=""
+                class="me-4 w-[45px] h-[45px]"
+              />
+              <div class="d-flex flex-column justify-content-center">
+                <h3 class="fs-6 fw-bold mb-2">Chat center</h3>
+                <p class="">
+                  Respond to clients in different messengers from a single
+                  window.
+                </p>
+              </div>
             </div>
           </div>
         </div>
