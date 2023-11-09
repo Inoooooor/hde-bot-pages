7bd539f6a6dac68044abf414b60faf0aeabe2ba1
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-11-09 12:46:48 +0300

    messangers-screen-content-fill

--- src/components/SecondScreen.vue
+++ src/components/SecondScreen.vue
@@ -8,12 +8,10 @@
       class="container"
     >
       <div class="text-center">
-        <h2 class="fs-3 fw-bold">
-          Automate and grow your business with BotHelp
-        </h2>
+        <h2 class="fs-3 fw-bold">Общайтесь с клиентами где им комфортно!</h2>
         <p class="py-4 text-base lg:text-xl text-gray-500">
-          We support WhatsApp, Instagram, Facebook Messenger, Telegram and
-          Viber.
+          Мы поддерживаем каналы ВК, Instagram, Facebook Messenger, Telegram,
+          Viber и онлайн-чат.
         </p>
         <SecondScreenMessengers />
       </div>
