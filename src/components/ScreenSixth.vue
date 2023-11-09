a16651bc50f3ef641717784c2140965281563d46
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-11-09 13:31:16 +0300

    offer-screen-text-content

--- src/components/ScreenSixth.vue
+++ src/components/ScreenSixth.vue
@@ -3,11 +3,19 @@
     <div class="container">
       <div class="row">
         <div class="text-center">
-          <h2 class="fs-3 fw-bold">Choose a plan that's right for you</h2>
+          <h2 class="fs-3 fw-bold">
+            Начните пользоваться HelpDeskEddy уже сегодня
+          </h2>
           <p class="py-4 fs-6 text-gray-500">
-            You start for free and pay as you go, with honest pricing that
-            scales as you grow. BotHelp accounts are billed based on the number
-            of active subscribers. Tools, messages and agents are unlimited.
+            Получите бесплатный доступ на 14 дней без ограничения функционала.
+            Стоимость формируется исходя из количества сотрудников в вашей
+            системе: 1 сотрудник = ₽1500 в месяц (или ₽1000 в год). За каждого
+            сотрудника вы получаете 250 бесплатных заявок в месяц, которые могут
+            быть обработаны ботом. Например, если у вас в системе 10
+            сотрудников, значит бот может бесплатно обработать 2500 заявок в
+            месяц. Если вы выходите за установленные лимиты, то можно докупить
+            пакет на 5000 заявок за ₽15000 – эти заявки не имеют ограничений по
+            времени и остаются с вами до окончания всего пакета!
           </p>
         </div>
       </div>
