a071143c2ea9574b3a0ae9fea49e5f830104345a
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-11-10 12:40:07 +0300

    fixed-btn-widths

--- src/components/UI/FreeTrialButton.vue
+++ src/components/UI/FreeTrialButton.vue
@@ -1,7 +1,7 @@
 <template>
   <el-button
     color="#23879A"
-    class="col-7 fs-5 p-4 rounded-3"
+    class="col-9 fs-5 p-4 rounded-3"
     >Попробовать бесплатно
   </el-button>
 </template>
--- src/components/UI/HeroButton.vue
+++ src/components/UI/HeroButton.vue
@@ -2,7 +2,7 @@
   <el-button
     color="#23879A"
     plain
-    class="col-8 fs-5 p-4 rounded-3 border col-sm-10"
+    class="col-9 fs-5 p-4 rounded-3 border col-sm-10"
     >Попробовать бесплатно
   </el-button>
 </template>
